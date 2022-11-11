5a19531b265d7a08845273f746712198ac84d25e
Lawand Othman <lwnd@pm.me>
2022-11-11 06:46:26 +0000

    Feat: sidebar component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,10 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
-import { trpc } from "../utils/trpc";
+import { trpc } from "utils/trpc";
+import { Sidebar } from "components/Sidebar";
 
-import "../styles/globals.css";
+import "styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,6 +13,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Sidebar />
       <Component {...pageProps} />
     </SessionProvider>
   );
