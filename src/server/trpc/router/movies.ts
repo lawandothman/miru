b5effbcd6804100ba286cce6a20ccb85e76dc15e
Lawand Othman <lwnd@pm.me>
2022-11-12 03:13:05 +0000

    Feat: fetch and display top rated movies

--- src/server/trpc/router/movies.ts
+++ src/server/trpc/router/movies.ts
@@ -1,5 +1,9 @@
 import tmdbAPI from "services/tmdbAPI";
-import type { GenreResponseType, PopularResponseType } from "types/tmdbAPI";
+import type {
+  GenreResponseType,
+  PopularResponseType,
+  TopRatedResponseType,
+} from "types/tmdbAPI";
 import { z } from "zod";
 import { publicProcedure, router } from "../trpc";
 
@@ -25,7 +29,24 @@ export const moviesRouter = router({
             page: input.page,
           },
         });
-        console.log(res.data);
+        return res.data;
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+  getTopRated: publicProcedure
+    .input(
+      z.object({
+        page: z.number().optional(),
+      })
+    )
+    .query<TopRatedResponseType>(async ({ input }) => {
+      try {
+        const res = await tmdbAPI.get("/3/movie/top_rated", {
+          params: {
+            page: input.page,
+          },
+        });
         return res.data;
       } catch (error) {
         console.log(error);
