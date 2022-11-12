6ba856a696f8599c332d787f9ed63b932fa39069
Lawand Othman <lwnd@pm.me>
2022-11-12 04:41:50 +0000

    By Genre (#21)
    * Feat: fetch and display movies by genre
    
    * Feat: handle loading state

---
+++ src/pages/genre/[id].tsx
@@ -0,0 +1,43 @@
+import { MoviesList } from "components/MoviesList";
+import { PageHeader } from "components/PageHeader";
+import { Pagination } from "components/Pagination";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { trpc } from "utils/trpc";
+
+const Genre: NextPage = () => {
+  const { query } = useRouter();
+  const genreId = Array.isArray(query.id) ? Number(query.id) : Number(query.id);
+  const page = Array.isArray(query.page)
+    ? Number(query.page[0])
+    : Number(query.page ?? 1);
+
+  const { data, isLoading: moviesLoading } = trpc.movies.getByGenre.useQuery({
+    id: genreId,
+    page,
+  });
+
+  const { data: genresData, isLoading: genresLoading } =
+    trpc.movies.getGenres.useQuery();
+
+  const isLoading = genresLoading || moviesLoading;
+
+  if (isLoading) {
+    return <div className="text-3xl text-white">LOADING</div>;
+  }
+
+  return (
+    <div className="px-20 pt-20">
+      <PageHeader
+        title={
+          genresData?.genres.filter((genre) => genre.id === genreId)[0]?.name ??
+          "Genre"
+        }
+      />
+      <MoviesList movies={data?.results} />
+      <Pagination page={page} />
+    </div>
+  );
+};
+
+export default Genre;
