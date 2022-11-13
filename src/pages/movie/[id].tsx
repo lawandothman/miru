87e2e803aa0af2c0780646537506d360c72d2075
Lawand Othman <lwnd@pm.me>
2022-11-13 20:23:33 +0000

    Movie Details (#22)
    * Deps: install data-fns
    
    * Feat: add the lang html attribute
    
    * Feat: Movie detail response type
    
    * Feat: getDetails movie procedure
    
    * Feat: movie detail page

---
+++ src/pages/movie/[id].tsx
@@ -0,0 +1,101 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { getImage } from "utils/image";
+import { trpc } from "utils/trpc";
+import { getYear } from "date-fns";
+import { FiLink, FiYoutube } from "react-icons/fi";
+import { FaImdb } from "react-icons/fa";
+
+const Movie: NextPage = () => {
+  const { query } = useRouter();
+  const movieId = Array.isArray(query.id)
+    ? Number(query.id[0])
+    : Number(query.id);
+
+  const { data: movie } = trpc.movies.getDetails.useQuery({ id: movieId });
+
+  const trailer = movie?.videos?.results?.find(
+    (video) => video.type === "Trailer" && video.site === "YouTube"
+  );
+
+  return (
+    <div className="flex flex-col px-8 pt-10">
+      <div className="mx-auto flex flex-col gap-24 pt-10 md:flex-row">
+        {movie?.poster_path && (
+          <Image
+            src={getImage(movie.poster_path)}
+            alt={movie.title ?? "movie"}
+            width={450}
+            height={1000}
+          />
+        )}
+        <div className="flex flex-col dark:text-white">
+          <h1 className="text-4xl font-thin uppercase tracking-widest ">
+            {movie?.title}
+          </h1>
+          <p className="mt-2 text-xl font-thin">{movie?.tagline}</p>
+          <div className="mt-6 text-sm text-neutral-400">
+            {movie?.spoken_languages?.map((lang) => (
+              <span key={lang.iso_639_1}>{lang.name} / </span>
+            ))}
+            <span>
+              {movie?.runtime && movie.runtime + " MIN / "}
+              {movie?.release_date && getYear(new Date(movie?.release_date))}
+            </span>
+            <div className="mt-8 flex gap-3">
+              {movie?.genres?.map((genre) => (
+                <Link
+                  href={`/genre/${genre.id}`}
+                  className="rounded border border-neutral-400 p-2 text-xs tracking-wide"
+                  key={genre.id}
+                >
+                  {genre.name}
+                </Link>
+              ))}
+            </div>
+            <p className="mt-8 max-w-xl text-neutral-400">{movie?.overview}</p>
+            <div className=" mt-6 flex gap-3">
+              {movie?.homepage && (
+                <Link
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  href={movie?.homepage}
+                  className="inline-flex items-center gap-2"
+                >
+                  <FiLink size={20} />
+                  Homepage
+                </Link>
+              )}
+              {movie?.imdb_id && (
+                <Link
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  href={`https://imdb.com/title/${movie.imdb_id}`}
+                  className="inline-flex items-center justify-center gap-2"
+                >
+                  <FaImdb size={20} />
+                  IMDB
+                </Link>
+              )}
+              {trailer && (
+                <Link
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  href={`https://youtube.com/watch?v=${trailer.key}`}
+                  className="inline-flex items-center justify-center gap-2"
+                >
+                  <FiYoutube size={20} />
+                  Trailer
+                </Link>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Movie;
--- src/types/tmdbAPI.d.ts
+++ src/types/tmdbAPI.d.ts
@@ -27,3 +27,72 @@ export type PaginatedMoviesResponseType = {
   total_results?: number;
   total_pages?: number;
 };
+
+interface ProductionCompany {
+  name?: string;
+  id?: number;
+  logo_path?: string | null;
+  origin_country?: string;
+}
+interface ProductionCountry {
+  iso_3166_1?: string;
+  name?: string;
+}
+interface SpokenLanguage {
+  iso_639_1?: string;
+  english_name?: string;
+  name?: string;
+}
+enum Status {
+  RUMORED = "Rumored",
+  PLANNED = "Planned",
+  IN_PRODUCTION = "In Production",
+  POST_PRODUCTION = "Post Production",
+  RELEASED = "Released",
+  CANCELLED = "Cancelled",
+}
+interface Movie {
+  adult?: boolean;
+  backdrop_path?: string | null;
+  belongs_to_collection?: object | null;
+  budge?: number;
+  genres?: Genre[];
+  homepage?: string | null;
+  id?: number;
+  imdb_id?: string | null;
+  original_language?: string;
+  original_title?: string;
+  overview?: string | null;
+  popularity?: number;
+  poster_path?: string | null;
+  production_companies?: ProductionCompany[];
+  production_countries?: ProductionCountry[];
+  release_date?: string;
+  revenue?: number;
+  runtime?: number | null;
+  spoken_languages?: SpokenLanguage[];
+  status?: Status;
+  tagline?: string | null;
+  title?: string;
+  video?: boolean;
+  vote_average?: number;
+  vote_count?: number;
+}
+interface Video {
+  iso_639_1?: string;
+  iso_3166_1?: string;
+  name?: string;
+  key?: string;
+  site?: string;
+  size?: number;
+  type?: string;
+  official?: boolean;
+  published_at?: string;
+  id?: string;
+}
+
+type MovieDetailResponseType = Movie & {
+  videos?: {
+    results?: Video[];
+  };
+};
