b273d828730ba519068188dec7f84b961d2d1d08
Lawand Othman <lwnd@pm.me>
2022-12-21 18:05:26 +0000

    pass token to apollo client requests

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,14 +1,35 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { getSession, SessionProvider } from "next-auth/react";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import { Sidebar } from "components/Sidebar";
 
 import "styles/globals.css";
 
-const client = new ApolloClient({
+const httpLinkt = createHttpLink({
   uri: "http://localhost:4000/graphql",
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const session = await getSession();
+
+  return {
+    headers: {
+      ...headers,
+      Authorization: session?.token ? `Bearer ${session.token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLinkt),
   cache: new InMemoryCache(),
 });
 
