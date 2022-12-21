6b4fae0f39e7f7c724c614a2cda30d190e88f7a2
Lawand Othman <lwnd@pm.me>
2022-12-21 18:05:14 +0000

    add token to the session

--- apps/frontend/src/pages/api/auth/[...nextauth].ts
+++ apps/frontend/src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
+import jsonwebtoken from "jsonwebtoken";
 import FacebookProvider from "next-auth/providers/facebook";
-
 
 import neo4j from "neo4j-driver";
 import { Neo4jAdapter } from "@next-auth/neo4j-adapter";
@@ -10,11 +10,19 @@ const driver = neo4j.driver("neo4j://localhost", neo4j.auth.basic("", ""));
 const neo4jSession = driver.session();
 
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
+  // Include user.id and jwt token on session
   callbacks: {
     session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
+        if (session.user.email) {
+          const encodedToken = jsonwebtoken.sign(
+            session.user.email,
+            process.env.NEXTAUTH_SECRET as string,
+            { algorithm: "HS256" }
+          );
+          session.token = encodedToken;
+        }
       }
       return session;
     },
