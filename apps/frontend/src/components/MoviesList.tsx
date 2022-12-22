2c2267ad589df78a1c081eb0ba7adfdd4f139f76
Lawand Othman <lwnd@pm.me>
2022-12-22 16:36:08 +0000

    ability to add/remove from watchlist in MovieList

--- apps/frontend/src/components/MoviesList.tsx
+++ apps/frontend/src/components/MoviesList.tsx
@@ -1,7 +1,9 @@
+import { gql, useMutation } from "@apollo/client";
 import Image from "next/image";
 import Link from "next/link";
 import type { FC } from "react";
 import { useState } from "react";
+import { FiMinus, FiPlus } from "react-icons/fi";
 import { cn } from "utils/cn";
 import { getImage } from "utils/image";
 import type { Movie } from "__generated__/resolvers-types";
@@ -9,6 +11,24 @@ import type { Movie } from "__generated__/resolvers-types";
 interface MoviesListProps {
   movies?: Movie[];
 }
+
+const ADD_TO_WATCHLIST = gql`
+  mutation AddMovieToWatchlist($movieId: ID!) {
+    addMovieToWatchlist(movieId: $movieId) {
+      id
+      inWatchlist
+    }
+  }
+`;
+
+const REMOVE_FROM_WATCHLIST = gql`
+  mutation RemoveMovieFromWatchlist($movieId: ID!) {
+    removeMovieFromWatchlist(movieId: $movieId) {
+      id
+      inWatchlist
+    }
+  }
+`;
 
 export const MoviesList: FC<MoviesListProps> = ({ movies }) => {
   return (
@@ -24,27 +44,87 @@ export const MoviesList: FC<MoviesListProps> = ({ movies }) => {
 
 const BlurImage = ({ movie }: { movie: Movie }) => {
   const [isLoading, setLoading] = useState(true);
+  const [addToWatchlist, {client: addClient}] = useMutation<Movie, { movieId?: string }>(
+    ADD_TO_WATCHLIST
+  );
+  const [removeFromWatchlist, {client: removeClient}] = useMutation<Movie, { movieId?: string }>(
+    REMOVE_FROM_WATCHLIST
+  );
   return (
-    <Link href={`/movie/${movie.id}`} className="group h-full">
+    <div className="h-full w-full">
       <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 relative h-[500px] w-full overflow-hidden rounded-lg lg:h-[500px]">
         {movie.posterUrl && (
-          <Image
-            alt={movie.title ?? ""}
-            src={getImage(movie.posterUrl)}
-            layout="fill"
-            loading="lazy"
-            objectFit="cover"
-            className={cn(
-              "absolute top-0 left-0 bottom-0 right-0 min-h-full min-w-full duration-200 ease-in-out group-hover:scale-110",
-              isLoading ? "blur-2xl" : "blur-0"
-            )}
-            onLoadingComplete={() => setLoading(false)}
-          />
+          <Link href={`/movie/${movie.id}`}>
+            <Image
+              alt={movie.title ?? ""}
+              src={getImage(movie.posterUrl)}
+              blurDataURL={getImage(movie.posterUrl)}
+              fill
+              loading="lazy"
+              sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              25vw"
+              className={cn(
+                "absolute top-0 left-0 bottom-0 right-0 min-h-full min-w-full object-cover",
+                isLoading ? "animate-pulse bg-neutral-700" : "blur-0"
+              )}
+              onLoadingComplete={() => setLoading(false)}
+            />
+          </Link>
         )}
       </div>
-      <h3 className="mt-4 text-center text-sm dark:text-neutral-300">
-        {movie.title}
-      </h3>
-    </Link>
+      {isLoading ? (
+        <div className="mx-auto mt-4 h-2.5 w-48 animate-pulse rounded-full bg-gray-200 dark:bg-neutral-700"></div>
+      ) : (
+        <h3 className="mt-4 text-center text-sm dark:text-neutral-300">
+          {movie.title}
+        </h3>
+      )}
+
+      <button
+        onClick={() => {
+          const movieId = movie.id;
+          if (movie.inWatchlist) {
+            removeFromWatchlist({
+              variables: {
+                movieId,
+              },
+            });
+            removeClient.writeFragment({
+              id: `Movie:${movieId}`,
+              fragment: gql`
+                fragment REMOVED on Movie {
+                  inWatchlist
+                }
+              `,
+              data: {
+                inWatchlist: false,
+              },
+            });
+          } else {
+            addToWatchlist({
+              variables: {
+                movieId,
+              },
+            });
+            addClient.writeFragment({
+              id: `Movie:${movieId}`,
+              fragment: gql`
+                fragment REMOVED on Movie {
+                  inWatchlist
+                }
+              `,
+              data: {
+                inWatchlist: true,
+              },
+            });
+          }
+        }}
+        className="mx-auto mb-8 mt-4 flex h-8 w-36 items-center justify-center gap-1 rounded-md border border-neutral-500 dark:text-neutral-300"
+      >
+        {movie.inWatchlist ? <FiMinus /> : <FiPlus />}
+        Watchlist
+      </button>
+    </div>
   );
 };
