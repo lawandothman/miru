e8627a187bae98fef67a5ece657c678feff5eccb
Pedro Santos de Mendonca <peumendonca@gmail.com>
2022-12-22 00:14:36 +0000

    Make everything configurable (#79)
    * Make everything configurable
    
    * Update config.ts

---
+++ apps/omni/src/config.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import env from 'env-var'
+
+dotenv.config()
+
+export const config = {
+  neo4j: {
+    host: env.get('NEO4J_HOST').required().asString(),
+    user: env.get('NEO4J_USER').required().asString(),
+    pass: env.get('NEO4J_PASS').required().asString(),
+  },
+  jwtSecret: env.get('OMNI_SECRET').required().asString(),
+  tmdb: {
+    readToken: env.get('TMDB_API_READ_ACCESS_TOKEN').required().asString(),
+    baseUrl: env.get('TMDB_API_BASE_URL').required().asString()
+  }
+}
