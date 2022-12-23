6f23741eafc0124ffc3f37fe58d9841c2cce6e99
Lawand Othman <lwnd@pm.me>
2022-12-23 22:38:34 +0000

    typo

--- apps/frontend/next-seo.config.ts
+++ apps/frontend/next-seo.config.ts
@@ -3,7 +3,7 @@ import type { DefaultSeoProps } from "next-seo";
 const config: DefaultSeoProps = {
   title: "ミル Miru",
   description:
-    "Miru helps you remove the drama from movie night with and find the movie that everyone wants to watch.",
+    "Miru helps you remove the drama from movie night and find the movie that everyone wants to watch.",
   canonical: "https://www.miru-chi.vercel.app/",
   openGraph: {
     url: "https://www.miru-chi.vercel.app/",
