8e43ba78b01b726bb3e2ae57564d4723e1562716
Lawand Othman <lwnd@pm.me>
2022-12-23 17:34:08 +0000

    set up next-seo

---
+++ apps/frontend/next-seo.config.ts
@@ -0,0 +1,16 @@
+import type { DefaultSeoProps } from "next-seo";
+
+const config: DefaultSeoProps = {
+  title: "ミル Miru",
+  description:
+    "Miru helps you remove the drama from movie night with and find the movie that everyone wants to watch.",
+  canonical: "https://www.miru-chi.vercel.app/",
+  openGraph: {
+    url: "https://www.miru-chi.vercel.app/",
+    type: "website",
+    locale: "en_IE",
+    siteName: "ミル Miru",
+  },
+};
+
+export default config;
