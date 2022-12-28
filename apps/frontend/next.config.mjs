c89501fe86980b127b2e9499b51734fca808b901
Lawand Othman <lwnd@pm.me>
2022-12-28 19:04:18 +0000

    Add sentry frontend  (#120)

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,17 +1,41 @@
+import { withSentryConfig } from '@sentry/nextjs'
+
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
   images: {
     remotePatterns: [
       {
-        hostname: "image.tmdb.org",
+        hostname: 'image.tmdb.org',
       },
     ],
   },
-};
-export default config;
+  sentry: {
+    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
+    // for client-side builds. (This will be the default starting in
+    // `@sentry/nextjs` version 8.0.0.) See
+    // https://webpack.js.org/configuration/devtool/ and
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
+    // for more information.
+    hideSourceMaps: true,
+  },
+}
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+}
+
+export default withSentryConfig(config, sentryWebpackPluginOptions)
