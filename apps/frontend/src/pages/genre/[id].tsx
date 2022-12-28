b3cc5a6c132aa2d7b6a341dec9f2c4d94f9fd60a
Lawand Othman <lwnd@pm.me>
2022-12-28 20:41:47 +0000

    get genre

--- apps/frontend/src/pages/genre/[id].tsx
+++ apps/frontend/src/pages/genre/[id].tsx
@@ -1,9 +1,11 @@
 import { gql, useQuery } from '@apollo/client'
-import { LoadingSkeleton, MoviesList } from 'components/MoviesList'
+import { MoviesList } from 'components/MoviesList'
 import { PageHeader } from 'components/PageHeader'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { Genre } from '__generated__/resolvers-types'
 import type { Movie } from '__generated__/resolvers-types'
+import { FullPageLoader } from 'components/FullPageLoader'
 
 const GET_BY_GENRE = gql`
   query MoviesByGenre($genreId: ID!) {
@@ -11,10 +13,10 @@ const GET_BY_GENRE = gql`
       id
       title
       posterUrl
-      genres {
-        name
-      }
       inWatchlist
+    }
+    genre(genreId: $genreId) {
+      name
     }
   }
 `
@@ -23,7 +25,7 @@ const Genre: NextPage = () => {
   const { query } = useRouter()
   const genreId = Array.isArray(query.id) ? query.id[0] : query.id
   const { data, loading } = useQuery<
-  { moviesByGenre: Movie[] },
+  { moviesByGenre: Movie[]; genre: Genre },
   { genreId?: string }
   >(GET_BY_GENRE, {
     variables: {
@@ -31,14 +33,14 @@ const Genre: NextPage = () => {
     },
   })
 
+  if (loading) {
+    return <FullPageLoader />
+  }
+
   return (
     <div className='px-20 pt-20'>
-      <PageHeader title={'Genre'} />
-      {loading ? (
-        <LoadingSkeleton />
-      ) : (
-        <MoviesList movies={data?.moviesByGenre} />
-      )}
+      <PageHeader title={data?.genre.name ?? ''} />
+      <MoviesList movies={data?.moviesByGenre} />
     </div>
   )
 }
