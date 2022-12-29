ce08efc74f02fb15b2620f3f01e01765430fc3e7
Lawand Othman <lwnd@pm.me>
2022-12-29 21:14:51 +0000

    about and privacy policy pages

---
+++ apps/frontend/src/pages/about.tsx
@@ -0,0 +1,66 @@
+import { PageHeader } from 'components/PageHeader'
+import type { NextPage } from 'next'
+import Link from 'next/link'
+
+const About: NextPage = () => {
+  return (
+    <div className='mx-auto max-w-4xl px-20 pt-20 text-white'>
+      <PageHeader title='Welcome to Miru!' subtitle='' />
+      <div className=' mx-auto flex flex-col gap-8 text-neutral-400'>
+        <p>
+          Miru is a platform that helps you and your friends find movies to
+          watch together. We know that one of the best parts of watching movies
+          is sharing the experience with others, and Miru makes it easy to find
+          the perfect film for your group.
+        </p>
+        <p>
+          Miru is designed to help you and your friends find the perfect movie
+          to watch together. One way we do this is by comparing your watchlists
+          and identifying common movies that you both want to see. This can be
+          especially helpful if you and your friends have different tastes in
+          movies, as it allows you to find a film that you can both agree on.
+        </p>
+        <p>
+          To use this feature, simply add Movies that you want to see to your
+          watchlist and invite your friends to do the same. Miru will then
+          analyze your lists and suggest movies that both of you have added.
+        </p>
+        <p>
+          So why wait? Start using Miru today and never watch a movie alone
+          again!
+        </p>
+      </div>
+      <h1 className='mb-4 mt-8 text-2xl font-thin tracking-widest dark:text-white'>
+        Acknowledgments
+      </h1>
+      <div className=' mx-auto flex flex-col gap-8 dark:text-neutral-400'>
+        <p>
+          This product uses the{' '}
+          <Link href='https://www.themoviedb.org/' className='text-white'>
+            TMDB API
+          </Link>{' '}
+          (The Movie Database API) but is not endorsed or certified by TMDb.
+          TMDb is a popular movie database that provides a wealth of information
+          on films, including plot summaries, cast and crew lists, and more. We
+          utilize their API to provide movie recommendations and information to
+          our users, but we are not affiliated with TMDb in any official
+          capacity.
+        </p>
+        <p>
+          We offer a variety of options for finding films that fit your
+          preferences and interests, including information on where you can
+          stream, rent, or buy a movie. This data comes from{' '}
+          <Link href='https://www.justwatch.com/' className='text-white'>
+            JustWatch
+          </Link>
+          , a leading platform for finding the best way to watch movies and TV
+          shows online. Currently, the information provided by JustWatch is
+          specific to the UK, but we plan to expand to other regions in the
+          future.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About
--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -10,16 +10,8 @@ import Image from 'next/image'
 import PhoneImg from '../../public/illustration/dark/phone.png'
 import React from 'react'
 import { FiSearch, FiShuffle, FiUserPlus } from 'react-icons/fi'
-import { IconType } from 'react-icons/lib'
+import type { IconType } from 'react-icons/lib'
 import type { User } from '__generated__/resolvers-types'
-
-export const GET_WATCHLIST = gql`
-  query Watchlist {
-    watchlist {
-      id
-    }
-  }
-`
 
 const GET_HOME = gql`
   query ($userId: ID!) {
@@ -44,33 +36,39 @@ const Home: NextPage = () => {
   const { data: session, status: sessionStatus } = useSession()
   const { data, loading } = useQuery<{ user: User }>(GET_HOME, {
     variables: { userId: session?.user?.id },
-    fetchPolicy: 'network-only'
+    fetchPolicy: 'network-only',
   })
   if (sessionStatus === 'loading' || loading) {
     return <FullPageLoader />
   }
   if (!session) {
-    return loggedOutPage()
+    return <LoggedOutPage />
   }
 
   return (
     <div className='px-20 pt-20'>
       <PageHeader title='Welcome back!' subtitle='' />
-      {sortBy(data?.user.following, [(u => -(u?.matches?.length ?? 0) )]).map((following) => {
-        if (following) {
-          return <UserSummary key={following.id} user={following} />
-        } else {
-          return null
+      {sortBy(data?.user.following, [(u) => -(u?.matches?.length ?? 0)]).map(
+        (following) => {
+          if (following) {
+            return <UserSummary key={following.id} user={following} />
+          } else {
+            return null
+          }
         }
-      })}
+      )}
+      <Footer />
     </div>
   )
 }
 
-const loggedOutPage = () => {
+const LoggedOutPage = () => {
   return (
-    <div className='px-20 pt-20 mx-auto max-w-4xl text-white'>
-      <PageHeader title='Welcome to Miru' subtitle='The social movie watching platform'></PageHeader>
+    <div className='mx-auto max-w-4xl px-20 pt-20 text-white'>
+      <PageHeader
+        title='Welcome to Miru'
+        subtitle='The social movie watching platform'
+      ></PageHeader>
       <p className='text-neutral-300'>
         Remove the drama from movie night and find the movie that everyone wants
         to watch.
@@ -81,28 +79,58 @@ const loggedOutPage = () => {
 
       <Image className='mx-auto' src={PhoneImg} alt={'Illustration'}></Image>
 
-      <h2 className='text-xl mt-4'>How it works?</h2>
+      <h2 className='mt-4 text-xl'>How it works?</h2>
       <div className='grid grid-cols-1 gap-4 md:grid-cols-3'>
-        <Step icon={FiUserPlus} text={'Find your friends'} description={'Follow your friends in Miru and we will recommend movies for you to watch together.'} />
-        <Step icon={FiSearch} text={'Find your movies'} description={'Search for movies you want to watch, or just check your For you page for recommendations based on the people you follow.'} />
-        <Step icon={FiShuffle} text={'Find your matches'} description={'Miru will match you and the people you follow to show what you should watch together'} />
+        <Step
+          icon={FiUserPlus}
+          text={'Find your friends'}
+          description={
+            'Follow your friends in Miru and we will recommend movies for you to watch together.'
+          }
+        />
+        <Step
+          icon={FiSearch}
+          text={'Find your movies'}
+          description={
+            'Search for movies you want to watch, or just check your For you page for recommendations based on the people you follow.'
+          }
+        />
+        <Step
+          icon={FiShuffle}
+          text={'Find your matches'}
+          description={
+            'Miru will match you and the people you follow to show what you should watch together'
+          }
+        />
       </div>
 
       <Link
         href='/auth/signin'
-        className='block text-center mt-12 p-2 text-white rounded-md bg-neutral-900 dark:bg-neutral-300 text-black'
+        className='mx-auto mt-12 block max-w-lg rounded-md bg-neutral-900 px-2 py-4 text-center text-lg font-semibold  text-white dark:bg-neutral-300 dark:text-black'
       >
         Login
       </Link>
+      <Footer />
     </div>
   )
 }
 
-function Step({ icon, text, description }: { icon: IconType, text: string, description: string }) {
+const Step = ({
+  icon,
+  text,
+  description,
+}: {
+  icon: IconType;
+  text: string;
+  description: string;
+}) => {
   return (
     <div>
-      <div className='w-full flex justify-center'>
-        {React.createElement(icon, { className: 'h-24 w-24 text-4xl bg-neutral-900 border-full rounded-full p-8 m-4' })}
+      <div className='flex w-full justify-center'>
+        {React.createElement(icon, {
+          className:
+            'h-24 w-24 text-4xl bg-neutral-900 border-full rounded-full p-8 m-4',
+        })}
       </div>
       <div className='w-full'>
         <p className='text-l text-center font-bold'>{text}</p>
@@ -111,5 +139,18 @@ function Step({ icon, text, description }: { icon: IconType, text: string, descr
     </div>
   )
 }
+
+const Footer = () => (
+  <div className='mt-12 mb-2 flex w-full gap-2  text-sm text-neutral-500'>
+    <Link href='/about' className='hover:text-white'>
+          About
+    </Link>
+    <span>•</span>
+    <Link href='/privacy' className='hover:text-white'>
+          Privacy Policy
+    </Link>
+  </div>
+)
+
 
 export default Home
