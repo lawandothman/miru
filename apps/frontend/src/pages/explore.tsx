bea994a0002fa2428547988686808c7ab5535d5b
Lawand Othman <lwnd@pm.me>
2023-01-05 19:12:07 +0000

    2 columns on mobile for explore

--- apps/frontend/src/pages/explore.tsx
+++ apps/frontend/src/pages/explore.tsx
@@ -141,7 +141,7 @@ const Search: NextPage = () => {
     return (
       <ExploreSkeleton>
         {emptyStateData?.genres && (
-          <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4'>
+          <div className='grid grid-cols-2 gap-4 lg:grid-cols-4'>
             {emptyStateData?.genres.map((genre) => (
               <Link
                 className='rounded-lg bg-neutral-800 p-8 text-center'
