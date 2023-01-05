2b8aa97da54bf9cfcd12903c7c7d2bdfeb05f89c
Lawand Othman <lwnd@pm.me>
2023-01-05 18:43:48 +0000

    PWA (#155)

--- apps/frontend/src/pages/_document.tsx
+++ apps/frontend/src/pages/_document.tsx
@@ -4,11 +4,27 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/apple-touch-icon.png'
+        <meta charSet='utf-8' />
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <link rel='manifest' href='/site.webmanifest' />
+        <meta name='application-name' content='Miru' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+        <meta name='apple-mobile-web-app-title' content='Miru' />
+        <meta name='format-detection' content='telephone=no' />
+        <meta name='mobile-web-app-capable' content='yes' />
+        <meta
+          name='theme-color'
+          media='(prefers-color-scheme: light)'
+          content='#FFFFFF'
         />
+        <meta
+          name='theme-color'
+          media='(prefers-color-scheme: dark)'
+          content='#000000'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='apple-touch-icon' href='/touch-icon-iphone.png' />
         <link
           rel='icon'
           type='image/png'
@@ -21,8 +37,7 @@ export default function Document() {
           sizes='16x16'
           href='/favicon-16x16.png'
         />
-        <link rel='icon' href='/favicon.ico' />
-        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='manifest' href='/manifest.json' />
       </Head>
       <body className='dark:bg-black'>
         <Main />
