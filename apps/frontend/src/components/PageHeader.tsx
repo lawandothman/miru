43536b11917bb5cc83d15ebf9bc75631716fc6aa
Lawand Othman <lwnd@pm.me>
2023-01-17 12:34:20 +0000

    remove unnecessary styles

--- apps/frontend/src/components/PageHeader.tsx
+++ apps/frontend/src/components/PageHeader.tsx
@@ -4,16 +4,11 @@ interface PageHeaderProps {
   title: string;
   subtitle?: string;
 }
-export const PageHeader: FC<PageHeaderProps> = ({
-  title,
-  subtitle = '',
-}) => {
+export const PageHeader: FC<PageHeaderProps> = ({ title, subtitle }) => {
   return (
     <header className='mb-4'>
-      <h1 className='text-3xl font-thin tracking-widest dark:text-white'>
-        {title}
-      </h1>
-      <p className='mt-1 text-lg font-light dark:text-white'>{subtitle}</p>
+      <h1 className='text-3xl font-thin tracking-widest'>{title}</h1>
+      {subtitle && <p className='mt-1 text-lg font-light'>{subtitle}</p>}
     </header>
   )
 }
