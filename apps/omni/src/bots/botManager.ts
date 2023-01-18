afa7440e51f86fa0d660d5e48eb0c872de0a1665
Lawand Othman <lwnd@pm.me>
2023-01-18 01:06:03 +0000

    homework

--- apps/omni/src/bots/botManager.ts
+++ apps/omni/src/bots/botManager.ts
@@ -21,12 +21,48 @@ function createBot(id: string, name: string, image: string | null, movieIds: str
 
 
 const bots: Bot[] = [
-  createBot('avengers-guy', 'Avengers guy', null, []),
-  createBot('classics', 'I like the classics', null, []),
-  createBot('a24-guy', 'indie man', null, [
-    '798286', '376867', '493922', '530385', '264660', '558582'
+  createBot('avengers-guy', 'Avengers guy', null, [
+    '299536',
+    '24428',
+    '299534',
+    '99861',
+    '634649',
+    '315635',
+    '284054',
+    '102899',
+    '44912',
+    '1726',
+    '271110'
   ]),
-
+  createBot('classics', 'I like the classics', null, [
+    '770',
+    '278',
+    '597',
+    '238',
+    '185',
+    '103',
+    '1366',
+    '630',
+    '252',
+    '947'
+  ]),
+  createBot('a24-guy', 'Indie man', null, [
+    '798286',
+    '376867',
+    '493922',
+    '530385',
+    '264660',
+    '558582',
+    '615643',
+    '503919',
+    '429200',
+    '141',
+    '103',
+    '152601',
+    '502033',
+    '641',
+    '185'
+  ]),
 ]
 
 export class BotManager {
@@ -43,7 +79,7 @@ export class BotManager {
   private async upsertBotUser(bot: Bot): Promise<void> {
     const session = this.driver.session()
     await session.run(`
-      MERGE (bot:User:Bot {id: $id}) 
+      MERGE (bot:User:Bot {id: $id})
       SET bot.email = $email
       SET bot.image = $image
       SET bot.name = $name
