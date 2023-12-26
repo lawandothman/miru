48ba2109bd4a9510073c8ec26eb7362eb792918e
Pedro Santos de Mendonca <peumendonca@gmail.com>
2023-12-26 23:08:18 +0000

    Bind to 0.0.0.0

--- railway-entrypoint.sh
+++ railway-entrypoint.sh
@@ -1,5 +1,7 @@
 echo "Setting port config to $PORT"
-echo "\nserver.bolt.listen_address=:${PORT}" >> /var/lib/neo4j/conf/neo4j.conf
+rm /var/lib/neo4j/conf/neo4j.conf
+echo "\nserver.bolt.listen_address=0.0.0.0:${PORT}" >> /var/lib/neo4j/conf/neo4j.conf
+echo "\nserver.http.listen_address=0.0.0.0:7474" >> /var/lib/neo4j/conf/neo4j.conf
 
 echo "Starting Neo4j with config"	
 cat /var/lib/neo4j/conf/neo4j.conf
