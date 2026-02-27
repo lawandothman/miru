acc41967b8420255e9040bcaa4be89a08c83b0ec
Lawand Othman <lwnd@pm.me>
2026-02-27 18:15:23 +0000

    fmt

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -4,19 +4,27 @@ export default function TabLayout() {
 	return (
 		<NativeTabs>
 			<NativeTabs.Trigger name="index">
-				<NativeTabs.Trigger.Icon sf={{ default: "house", selected: "house.fill" }} />
+				<NativeTabs.Trigger.Icon
+					sf={{ default: "house", selected: "house.fill" }}
+				/>
 				<NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
 			</NativeTabs.Trigger>
 			<NativeTabs.Trigger name="discover">
-				<NativeTabs.Trigger.Icon sf={{ default: "safari", selected: "safari.fill" }} />
+				<NativeTabs.Trigger.Icon
+					sf={{ default: "safari", selected: "safari.fill" }}
+				/>
 				<NativeTabs.Trigger.Label>Discover</NativeTabs.Trigger.Label>
 			</NativeTabs.Trigger>
 			<NativeTabs.Trigger name="watchlist">
-				<NativeTabs.Trigger.Icon sf={{ default: "bookmark", selected: "bookmark.fill" }} />
+				<NativeTabs.Trigger.Icon
+					sf={{ default: "bookmark", selected: "bookmark.fill" }}
+				/>
 				<NativeTabs.Trigger.Label>Watchlist</NativeTabs.Trigger.Label>
 			</NativeTabs.Trigger>
 			<NativeTabs.Trigger name="profile">
-				<NativeTabs.Trigger.Icon sf={{ default: "person", selected: "person.fill" }} />
+				<NativeTabs.Trigger.Icon
+					sf={{ default: "person", selected: "person.fill" }}
+				/>
 				<NativeTabs.Trigger.Label>Profile</NativeTabs.Trigger.Label>
 			</NativeTabs.Trigger>
 		</NativeTabs>
