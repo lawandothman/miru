68320c8cd8f830a3bf19d382ecf8c1bcc83058b6
Lawand Othman <lwnd@pm.me>
2026-03-16 01:39:04 +0000

    fix

--- apps/mobile/src/lib/auth.ts
+++ apps/mobile/src/lib/auth.ts
@@ -1,4 +1,3 @@
-import { NativeModules, Platform } from "react-native";
 import { createAuthClient } from "better-auth/react";
 import { expoClient } from "@better-auth/expo/client";
 import * as SecureStore from "expo-secure-store";
