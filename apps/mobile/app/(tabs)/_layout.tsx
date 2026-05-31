3b5c2d4b8dcc001d3decbe1b99fff7acddc1e232
Lawand Othman <lwnd@pm.me>
2026-05-31 17:17:35 +0100

    fix nav tabs

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -6,28 +6,36 @@ export default function TabLayout() {
 			<NativeTabs.Trigger name="index">
 				<NativeTabs.Trigger.Icon
 					sf={{ default: "house", selected: "house.fill" }}
+					md="home"
 				/>
 				<NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
 			</NativeTabs.Trigger>
 			<NativeTabs.Trigger name="discover">
 				<NativeTabs.Trigger.Icon
 					sf={{ default: "safari", selected: "safari.fill" }}
+					md="explore"
 				/>
 				<NativeTabs.Trigger.Label>Discover</NativeTabs.Trigger.Label>
 			</NativeTabs.Trigger>
 			<NativeTabs.Trigger name="watchlist">
 				<NativeTabs.Trigger.Icon
 					sf={{ default: "bookmark", selected: "bookmark.fill" }}
+					md="bookmark"
 				/>
 				<NativeTabs.Trigger.Label>Watchlist</NativeTabs.Trigger.Label>
 			</NativeTabs.Trigger>
 			<NativeTabs.Trigger name="profile">
 				<NativeTabs.Trigger.Icon
 					sf={{ default: "person", selected: "person.fill" }}
+					md="person"
 				/>
 				<NativeTabs.Trigger.Label>Profile</NativeTabs.Trigger.Label>
 			</NativeTabs.Trigger>
 			<NativeTabs.Trigger name="search" role="search">
+				<NativeTabs.Trigger.Icon
+					sf="magnifyingglass"
+					md="search"
+				/>
 				<NativeTabs.Trigger.Label>Search</NativeTabs.Trigger.Label>
 			</NativeTabs.Trigger>
 		</NativeTabs>
