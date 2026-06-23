caf228b44303978705297a632029f73e017c3d91
Lawand Othman <lwnd@pm.me>
2026-06-23 10:19:45 +0100

    restrict expo app to ios and android platforms

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -56,6 +56,7 @@ const config: ExpoConfig = {
 	name: "Miru",
 	slug: "miru",
 	version: appVersion,
+	platforms: ["ios", "android"],
 	orientation: "portrait",
 	icon: "./assets/icon.png",
 	scheme: "miru",
